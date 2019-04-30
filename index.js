a9c0a816bff346b1baa355c0475dccd8eeae869a
safeerjan <Jan.Safeer@vistajet.com>
2019-04-30 12:56:39 +0500

    Node Callback Hell and Promises.

--- index.js
+++ index.js
@@ -6,57 +6,76 @@ const dbname = 'conFusion';
 const dboper = require('./operations');
 
 
-MongoClient.connect(url, (err, client) => {
+// MongoClient.connect(url, (err, client) => {
 
-  assert.equal(err, null);
+//   assert.equal(err, null);
+
+//   console.log('Connected correctly to server');
+
+//   const db = client.db(dbname);
+
+//   dboper.insertDocument(db, { name: "Vadonut", description: "Test" },
+//     "dishes", (result) => {
+//       console.log("Insert Document:\n", result.ops);
+
+//       dboper.findDocuments(db, "dishes", (docs) => {
+//         console.log("Found Documents:\n", docs);
+
+//         dboper.updateDocument(db, { name: "Vadonut" },
+//           { description: "Updated Test" }, "dishes",
+//           (result) => {
+//             console.log("Updated Document:\n", result.result);
+
+//             dboper.findDocuments(db, "dishes", (docs) => {
+//               console.log("Found Updated Documents:\n", docs);
+
+//               db.dropCollection("dishes", (result) => {
+//                 console.log("Dropped Collection: ", result);
+
+//                 client.close();
+//               });
+//             });
+//           });
+//       });
+//     });
+// });
+
+//---------------------Now use promises instead of callback-----------------
+MongoClient.connect(url).then((client) => {
 
   console.log('Connected correctly to server');
-
   const db = client.db(dbname);
-  // const collection = db.collection("dishes");
-  // collection.insertOne({"name": "Uthappizza", "description": "test"},
-  // (err, result) => {
-  //     assert.equal(err,null);
-
-  //     console.log("After Insert:\n");
-  //     console.log(result.ops);
-
-  //     collection.find({}).toArray((err, docs) => {
-  //         assert.equal(err,null);
-
-  //         console.log("Found:\n");
-  //         console.log(docs);
-
-  //         db.dropCollection("dishes", (err, result) => {
-  //             assert.equal(err,null);
-
-  //             client.close();
-  //         });
-  //     });
-  // });
 
   dboper.insertDocument(db, { name: "Vadonut", description: "Test" },
-    "dishes", (result) => {
+    "dishes")
+    .then((result) => {
       console.log("Insert Document:\n", result.ops);
 
-      dboper.findDocuments(db, "dishes", (docs) => {
-        console.log("Found Documents:\n", docs);
+      return dboper.findDocuments(db, "dishes");
+    })
+    .then((docs) => {
+      console.log("Found Documents:\n", docs);
 
-        dboper.updateDocument(db, { name: "Vadonut" },
-          { description: "Updated Test" }, "dishes",
-          (result) => {
-            console.log("Updated Document:\n", result.result);
+      return dboper.updateDocument(db, { name: "Vadonut" },
+        { description: "Updated Test" }, "dishes");
 
-            dboper.findDocuments(db, "dishes", (docs) => {
-              console.log("Found Updated Documents:\n", docs);
+    })
+    .then((result) => {
+      console.log("Updated Document:\n", result.result);
 
-              db.dropCollection("dishes", (result) => {
-                console.log("Dropped Collection: ", result);
+      return dboper.findDocuments(db, "dishes");
+    })
+    .then((docs) => {
+      console.log("Found Updated Documents:\n", docs);
 
-                client.close();
-              });
-            });
-          });
-      });
-    });
-});
+      return db.dropCollection("dishes");
+    })
+    .then((result) => {
+      console.log("Dropped Collection: ", result);
+
+      return client.close();
+    })
+    .catch((err) => console.log(err));
+
+})
+  .catch((err) => console.log(err));
